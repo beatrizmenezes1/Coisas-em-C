6554b698e21a76239e84e31106dabc387020308c
Beatriz Menezes <beatrizmz.26menezes@gmail.com>
2021-09-02 16:21:52 -0400

    Create estrutura_DoWhile.c

---
+++ #HelpC/estrutura_DoWhile.c
@@ -0,0 +1,49 @@
+/*
+	#Help2
+	Estrutura de repetição - DO WHILE	
+
+*/
+
+//BIBLIOIECAS
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	int op; //variavel utilizada no loop
+	
+	do{ //inicio da estrutura
+		//menu simples
+		printf("---- MENU ----\n\n");
+		
+		printf("1- Hello World - Inglês\n");
+		printf("2- Óla Mundo - Português\n");
+		printf("3- Kon'nichiwa sekai - Japonês\n");
+		printf("0- Sair\n\n");
+		scanf("%d", &op);
+		
+		switch(op){
+			case 1:
+					printf("\tHello World\n\n");
+				break;
+			case 2:
+					printf("\tÓla Mundo\n\n");
+				break;
+			case 3:
+					printf("\tKon'nichiwa sekai\n\n");
+				break;
+			case 0:
+					printf("\tProcesso Finalizado\n\n");
+				break;
+			default:
+					printf("\tOpção Inválida\n\n");
+				break;
+		}
+		
+		
+	}while(op!=0); //caso variavel op for diferente de 0 entao o loop irá se repetir
+	//caso op for igual a 0 entao o loop é finalizado
+
+    return 0;
+}
