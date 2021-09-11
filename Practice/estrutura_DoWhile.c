9c2caad9acc4f7141f630144e16fcb7ef82331d3
Beatriz Menezes <beatrizmz.26menezes@gmail.com>
2021-09-11 17:54:47 -0400

    mudança

---
+++ Practice/estrutura_DoWhile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	int op; 
+	
+	do{
+		printf("---- MENU ----\n\n");
+		
+		printf("1- Hello World - Inglês\n");
+		printf("2- Óla Mundo - Português\n");
+		printf("3- Kon'nichiwa sekai - Japonês\n");
+		printf("0- Sair\n\n");
+		scanf("%d", &op);
+		
+		switch(op){
+			case 1:
+					printf("\tHello World\n\n");
+				break;
+			case 2:
+					printf("\tÓla Mundo\n\n");
+				break;
+			case 3:
+					printf("\tKon'nichiwa sekai\n\n");
+				break;
+			case 0:
+					printf("\tProcesso Finalizado\n\n");
+				break;
+			default:
+					printf("\tOpção Inválida\n\n");
+				break;
+		}
+		
+		
+	}while(op!=0); 
+
+    return 0;
+}
+
---
+++ Practice/estrutura_condicional.c
@@ -0,0 +1,63 @@
+#include <stdio.h> 
+#include <locale.h> 
+
+int main(){
+	setlocale(LC_ALL, "portuguese"); 
+	
+	int op, a, b, resultado; 
+	
+	printf("Escolha: \n\n");
+	printf("1- Somar\n");
+	printf("2- Subtrair\n");
+	scanf("%d", &op);
+
+	switch(op){ 
+	
+		case 1: 
+				
+				printf("Digite um número: ");
+				scanf("%d", &a);
+				printf("Digite mais um número: ");	
+				scanf("%d", &b);	
+						
+				resultado = a+b;
+				
+				printf("Resultado da soma: %d\n", resultado);
+				
+	
+				if(resultado>=0){ 
+					printf("Número é maior igual 0\n");
+				}
+				
+			break;
+			
+		case 2: 
+				
+				printf("Digite um número: ");
+				scanf("%d", &a);
+				printf("Digite mais um número: ");	
+				scanf("%d", &b);	
+						
+				resultado = a-b;
+				
+				printf("Resultado da subtração: %d\n", resultado);
+				
+				if(resultado>=0){
+					printf("Número é maior igual a 0\n");
+				}else{
+					printf("Número é menor 0\n"); 
+				}
+				
+				
+			break;
+			
+		default:
+				
+				printf("Opção Inválida!/n"); 
+			
+			break; 
+	}
+	
+	return 0;
+}
+
